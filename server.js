097e4eca1ac9848e459e1b1a03de04581412f326
Moul Channrith <moul.channrith@sabay.com>
2020-07-14 19:03:08 +0700

    push to all users

--- server.js
+++ server.js
@@ -6,6 +6,51 @@ const port = process.env.PORT || 3000;
 
 http.listen(port, () => {
     app.get('/', (req, res) => {
+
+
+
+        var sendNotification = function(data) {
+            var headers = {
+              "Content-Type": "application/json; charset=utf-8",
+              "Authorization": "Basic YmU4YzMzMDUtNjA3ZC00M2FhLWFiN2MtZWI0NTNlN2RhM2U1"
+            };
+            
+            var options = {
+              host: "onesignal.com",
+              port: 443,
+              path: "/api/v1/notifications",
+              method: "POST",
+              headers: headers
+            };
+            
+            var https = require('https');
+            var req = https.request(options, function(res) {  
+              res.on('data', function(data) {
+                console.log("Response:");
+                console.log(JSON.parse(data));
+              });
+            });
+            
+            req.on('error', function(e) {
+              console.log("ERROR:");
+              console.log(e);
+            });
+            
+            req.write(JSON.stringify(data));
+            req.end();
+          };
+          
+          var message = { 
+            app_id: "511bcbbb-ad80-438b-bd5c-2fbf2c0f4efc",
+            contents: {"en": "English Message"},
+            included_segments: ["All"]
+          };
+          
+          console.log(sendNotification(message));
+
+
+
+
         res.send('Hello world!');
     });
 });
